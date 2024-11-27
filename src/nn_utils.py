93dbd75c491871d0b821a2d26c676ee1f8837a7c
Tanguy Dusserre <tanguy.dusserre@universite-paris-saclay.fr>
2024-11-27 09:33:54 +0100

    Removed numpyro and JAX dependencies

--- src/nn_utils.py
+++ src/nn_utils.py
@@ -1,12 +1,12 @@
 # Fonctions utilisées par un réseau de neurones très simple (sortie booléenne)
 
 import numpy as np
-import jax.numpy as jnp
-from jax import grad, random
-import numpyro
-import numpyro.distributions as dist
-from numpyro.infer import NUTS, MCMC
-from numpyro.infer.initialization import init_to_value
+# import jax.numpy as jnp
+# from jax import grad, random
+# import numpyro
+# import numpyro.distributions as dist
+# from numpyro.infer import NUTS, MCMC
+# from numpyro.infer.initialization import init_to_value
 import progressbar
 from .activation_functions import *
 
@@ -71,87 +71,72 @@ class double_layer_nn:
     def output(self,x):
         out = self.neuron2.output(self.neuron1.output(x))
         return out
-        # if np.shape(out)==(1,1):
-        #     return out[0,0]
-        # else:
-        #     return out
     
     def new_output(self,x):
         new_neuron1 = neuron(self.new_weights()[0],self.new_biases()[0],self.activations[0])
         new_neuron2 = neuron(self.new_weights()[1],self.new_biases()[1],self.activations[1])
         out = new_neuron2.output(new_neuron1.output(x))
         return out
-        # if np.shape(out)==(1,1):
-        #     return out[0,0]
-        # else:
-        #     return out
 
-    def grad_backpropagation(self,score_func,data):
-        """
-        score_func must be written with jax.numpy instead of numpy
-        """
+    # def grad_backpropagation(self,score_func,data):
+    #     """
+    #     score_func must be written with jax.numpy instead of numpy
+    #     """
 
-        score_for_grad = lambda x, w1, w2, b1, b2: score_func( self.activations[1]( jnp.dot( self.activations[0]( jnp.dot ( x , w1 ) + b1 ) , w2 ) + b2) )
-        score_grad = grad(score_for_grad,argnums=[1,2,3,4])
-        score_grad_value = score_grad(data,self.weights[0],self.weights[1],self.biases[0],self.biases[1])
+    #     score_for_grad = lambda x, w1, w2, b1, b2: score_func( self.activations[1]( jnp.dot( self.activations[0]( jnp.dot ( x , w1 ) + b1 ) , w2 ) + b2) )
+    #     score_grad = grad(score_for_grad,argnums=[1,2,3,4])
+    #     score_grad_value = score_grad(data,self.weights[0],self.weights[1],self.biases[0],self.biases[1])
 
-        dw1 = score_grad_value[0]
-        dw2 = score_grad_value[1]
-        db1 = score_grad_value[2]
-        db2 = score_grad_value[3]
+    #     dw1 = score_grad_value[0]
+    #     dw2 = score_grad_value[1]
+    #     db1 = score_grad_value[2]
+    #     db2 = score_grad_value[3]
 
-        # print(f"{dw1} \n {dw2} \n {db1} \n {db2} \n")
+    #     new_w1 = self.weights[0] + dw1*self.learning_rate
+    #     new_w2 = self.weights[1] + dw2*self.learning_rate
+    #     new_b1 = self.biases[0] + db1*self.learning_rate
+    #     new_b2 = self.biases[1] + db2*self.learning_rate
 
-        new_w1 = self.weights[0] + dw1*self.learning_rate
-        new_w2 = self.weights[1] + dw2*self.learning_rate
-        new_b1 = self.biases[0] + db1*self.learning_rate
-        new_b2 = self.biases[1] + db2*self.learning_rate
-
-        # self.weights = [new_w1,new_w2]
-        # self.biases = [new_b1,new_b2]
-
-        return [new_w1,new_w2,new_b1,new_b2]
+    #     return [new_w1,new_w2,new_b1,new_b2]
     
-    def mse(self):
+    # def mse(self):
 
-        max_weight_range = 10
-        max_bias_range = 10
+    #     max_weight_range = 10
+    #     max_bias_range = 10
 
-        w1 = numpyro.sample('w1',dist.Uniform(low=jnp.zeros(jnp.shape(self.weights[0])),high=max_weight_range*jnp.ones(jnp.shape(self.weights[0]))))
-        w2 = numpyro.sample('w2',dist.Uniform(low=jnp.zeros(jnp.shape(self.weights[1])),high=max_weight_range*jnp.ones(jnp.shape(self.weights[1]))))
-        b1 = numpyro.sample('b1',dist.Uniform(low=-max_bias_range*jnp.ones(jnp.shape(self.biases[0])),high=max_bias_range*jnp.ones(jnp.shape(self.biases[0]))))
-        b2 = numpyro.sample('b2',dist.Uniform(low=-max_bias_range*jnp.ones(jnp.shape(self.biases[1])),high=max_bias_range*jnp.ones(jnp.shape(self.biases[1]))))
+    #     w1 = numpyro.sample('w1',dist.Uniform(low=jnp.zeros(jnp.shape(self.weights[0])),high=max_weight_range*jnp.ones(jnp.shape(self.weights[0]))))
+    #     w2 = numpyro.sample('w2',dist.Uniform(low=jnp.zeros(jnp.shape(self.weights[1])),high=max_weight_range*jnp.ones(jnp.shape(self.weights[1]))))
+    #     b1 = numpyro.sample('b1',dist.Uniform(low=-max_bias_range*jnp.ones(jnp.shape(self.biases[0])),high=max_bias_range*jnp.ones(jnp.shape(self.biases[0]))))
+    #     b2 = numpyro.sample('b2',dist.Uniform(low=-max_bias_range*jnp.ones(jnp.shape(self.biases[1])),high=max_bias_range*jnp.ones(jnp.shape(self.biases[1]))))
 
-        prop = self.activations[1]( jnp.dot( self.activations[0]( jnp.dot( self.data , w1 ) + b1 ) , w2 ) + b2 )
+    #     prop = self.activations[1]( jnp.dot( self.activations[0]( jnp.dot( self.data , w1 ) + b1 ) , w2 ) + b2 )
 
-        numpyro.sample('likelihood',dist.Normal(prop),obs=self.results)
+    #     numpyro.sample('likelihood',dist.Normal(prop),obs=self.results)
 
-        return
+    #     return
     
-    def MCMC(self,data,results):
+    # def MCMC(self,data,results):
         
-        self.data = data
-        self.results = results
-        self.n_trains = 2000
+    #     self.data = data
+    #     self.results = results
+    #     self.n_trains = 2000
 
-        init = {
-            'w1':self.weights[0],
-            'w2':self.weights[1],
-            'b1':self.biases[0],
-            'b2':self.biases[1]
-        }
+    #     init = {
+    #         'w1':self.weights[0],
+    #         'w2':self.weights[1],
+    #         'b1':self.biases[0],
+    #         'b2':self.biases[1]
+    #     }
 
-        rng_key = random.PRNGKey(0)
-        kernel = NUTS(self.mse,init_strategy=(init_to_value(values=init)))
-        mcmc = MCMC(kernel,num_warmup=self.n_trains//2,num_samples=self.n_trains//2,progress_bar=True)
-        mcmc.run(rng_key)
+    #     rng_key = random.PRNGKey(0)
+    #     kernel = NUTS(self.mse,init_strategy=(init_to_value(values=init)))
+    #     mcmc = MCMC(kernel,num_warmup=self.n_trains//2,num_samples=self.n_trains//2,progress_bar=True)
+    #     mcmc.run(rng_key)
 
-        chain = mcmc.get_samples()
+    #     chain = mcmc.get_samples()
 
-        return chain
+    #     return chain
     
-    # def score_for_grad(self,w1,w2,b1,b2,score_func):
-    #     return lambda x: score_func( self.activations[1]( np.dot( self.activations[0]( np.dot ( x , w1 ) + b1 ) , w2 ) + b2) )
     
 def backpropagation_MH_step(NN,score_func,step:int):
         """
